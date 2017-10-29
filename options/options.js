55fcdd6a129933d03d4fd64a74268bd6d2b55830
Alexandre Oliveira <rockytvbr@gmail.com>
2017-10-29 00:24:01 -0200

    Update options page style
    The reworked options page now uses jQuery and country-select-js and displays the country flag of the currencies in the dropdown.

--- options/options.js
+++ options/options.js
@@ -1,23 +1,45 @@
-(function() {
+(function () {
+    var cs = $('#currency').countrySelect({
+        preferredCountries: [],
+        onlyCountries: ['au', 'br', 'gb', 'ca', 'cl', 'cn', 'dk', 'eu', 'hk', 'in', 'is', 'jp', 'nz', 'pl', 'ru', 'si', 'kr', 'se', 'ch', 'tw', 'ta', 'us'],
+        responsiveDropdown: true
+    });
+
+    var currencies = $.fn.countrySelect.getCountryData();
+
     function saveOptions() {
-        setOption('currency', document.querySelector('#currency').value);
+        setOption('currency', $('#currency').countrySelect('getSelectedCountryData').currency);
     }
 
     function setOption(option, value) {
         var yabpt = {};
         yabpt[option] = value;
 
-        chrome.storage.local.set({yabpt: yabpt});
+        chrome.storage.local.set({
+            yabpt: yabpt
+        });
     }
 
     function restoreOptions() {
         document.querySelector('#setting_description span.title').textContent = chrome.i18n.getMessage("optionCurrency");
         document.querySelector('#setting_description description').textContent = chrome.i18n.getMessage("optionCurrencyDesc");
-        chrome.storage.local.get('yabpt', function(res) {
-            document.querySelector('#currency').value = res.yabpt.currency || 'USD';
+        chrome.storage.local.get('yabpt', function (res) {
+            // Need to adapt to the new system
+            if (res.yabpt.currency.length == 3) {
+                var prevOption = res.yabpt.currency;
+                var selectedCountry = 'us';
+                $.each(currencies, function (idx, c) {
+                    if (prevOption == c.currency) {
+                        selectedCountry = c.iso2;
+                    }
+                });
+                $('#currency').countrySelect('selectCountry', selectedCountry);
+            }
         });
     }
 
     document.addEventListener('DOMContentLoaded', restoreOptions);
-    document.querySelector('#currency').addEventListener('change', saveOptions);
+    $('#currency').change(function () {
+        saveOptions();
+    })
 })();
