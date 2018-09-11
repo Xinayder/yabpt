e01e5acda5fb333e54a0445b700144cbbb0741d5
Alexandre Oliveira <rockytvbr@gmail.com>
2018-09-10 23:10:12 -0300

    Add web-ext config file

---
+++ web-ext-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    ignoreFiles: [
+        'npm-debug.log',
+        'crowdin.yml',
+        'web-ext-artifcats',
+        'web-ext-config.js'
+    ],
+    build: {
+        overwriteDest: true
+    }
+}
